8116f7682e7de65fd901e22de7d36efffff991be
maarten <maarten@eb168f56-b88a-429f-bc04-022912e7fcb5>
2019-05-21 07:32:49 +0000

    die was ik vergeten
    git-svn-id: svn+ssh://s4.hekkelman.net/home/maarten/svnroot/energyd/branches/v4@513 eb168f56-b88a-429f-bc04-022912e7fcb5

---
+++ include/zeep/json/from_element.hpp
@@ -0,0 +1,164 @@
+//          Copyright Maarten L. Hekkelman, 2019
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <cassert>
+#include <string>
+#include <memory>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <experimental/type_traits>
+
+#include <zeep/json/element_fwd.hpp>
+#include <zeep/json/traits.hpp>
+#include <zeep/json/factory.hpp>
+
+namespace zeep
+{
+namespace detail
+{
+
+template<typename E>
+void from_element(const E& e, std::nullptr_t& n)
+{
+    if (not e.is_null())
+        throw std::runtime_error("Type should have been null but was " + e.type_name());
+    n = nullptr;
+}
+
+template<typename E, typename T,
+    std::enable_if_t<std::is_arithmetic<T>::value and not std::is_same<T, bool>::value, int> = 0>
+void get_number(const E& e, T& v)
+{
+    switch (e.type())
+    {
+        case value_type::number_int:
+            v = static_cast<T>(*e.get_ptr<value_type::number_int>());
+            break;
+
+        case value_type::number_uint:
+            v = static_cast<T>(*e.get_ptr<value_type::number_uint>());
+            break;
+
+        case value_type::number_float:
+            v = static_cast<T>(*e.get_ptr<value_type::number_float>());
+            break;
+        
+        default:
+            throw std::runtime_error("Type should have been number but was " + e.type_name());
+            break;
+    }
+}
+
+template<typename E>
+void from_element(const E& e, bool& b)
+{
+    
+}
+
+
+// template<typename T, std::enable_if_t<std::is_same<T, bool>::value, int> = 0>
+// void to_element(element& v, T b)
+// {
+// 	factory<value_type::boolean>::construct(v, b);
+// }
+
+// template<typename T, std::enable_if_t<std::is_constructible<T, std::string>::value, int> = 0>
+// void to_element(element& v, const T& s)
+// {
+// 	factory<value_type::string>::construct(v, s);
+// }
+
+// inline void to_element(element& v, std::string&& s)
+// {
+// 	factory<value_type::string>::construct(v, std::move(s));
+// }
+
+// template<typename T, std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+// void to_element(element& v, T f)
+// {
+// 	factory<value_type::number_float>::construct(v, f);
+// }
+
+// template<typename T, std::enable_if_t<std::is_integral<T>::value and std::is_signed<T>::value and not std::is_same<T, bool>::value, int> = 0>
+// void to_element(element& v, T i)
+// {
+// 	factory<value_type::number_int>::construct(v, i);
+// }
+
+// template<typename T, std::enable_if_t<std::is_integral<T>::value and std::is_unsigned<T>::value and not std::is_same<T, bool>::value, int> = 0>
+// void to_element(element& v, T u)
+// {
+// 	factory<value_type::number_uint>::construct(v, u);
+// }
+
+// template<typename T, std::enable_if_t<std::is_enum<T>::value, int> = 0>
+// void to_element(element& v, T e)
+// {
+// 	using int_type = typename std::underlying_type<T>::type;
+// 	factory<value_type::number_int>::construct(v, static_cast<int_type>(e));
+// }
+
+// inline void to_element(element& j, const std::vector<bool>& v)
+// {
+// 	factory<value_type::array>::construct(j, v);
+// }
+
+// template<typename T, std::enable_if_t<is_array_type<T>::value, int> = 0>
+// void to_element(element& j, const T& arr)
+// {
+// 	factory<value_type::array>::construct(j, arr);
+// }
+
+// template<typename T, std::enable_if_t<std::is_convertible<element,T>::value, int> = 0>
+// void to_element(element& j, const std::valarray<T>& arr)
+// {
+// 	factory<value_type::array>::construct(j, std::move(arr));
+// }
+
+// template<typename J>
+// void to_element(element& j, const typename J::array_type& arr)
+// {
+// 	factory<value_type::array>::construct(j, std::move(arr));
+// }
+
+// template<typename J, typename T, size_t N,
+// 	std::enable_if_t<not std::is_constructible<typename J::string_type, const T(&)[N]>::value, int> = 0>
+// void to_element(J& j, const T(&arr)[N])
+// {
+// 	factory<value_type::array>::construct(j, std::move(arr));
+// } 
+
+// template<typename T, std::enable_if_t<is_object_type<element,T>::value, int> = 0>
+// void to_element(element& j, const T& obj)
+// {
+// 	factory<value_type::object>::construct(j, std::move(obj));
+// }
+
+// template<typename J>
+// void to_element(J& j, const J& obj)
+// {
+// 	factory<value_type::object>::construct(j, std::move(obj));
+// }
+
+// struct to_element_fn
+// {
+//     template<typename T>
+//     auto operator()(element& j, T&& val) const noexcept(noexcept(to_element(j, std::forward<T>(val))))
+//     -> decltype(to_element(j, std::forward<T>(val)), void())
+//     {
+//         return to_element(j, std::forward<T>(val));
+//     }
+// };
+
+// namespace
+// {
+//     constexpr const auto& to_element = typename ::zeep::detail::to_element_fn{};
+// }
+
+}
+}
